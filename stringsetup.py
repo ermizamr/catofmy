27003ae72a990f626ab07ca62b6865f4b8a21a39
ermizamr <89203924+ermizamr@users.noreply.github.com>
2022-12-11 22:49:48 +0300

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -8,13 +8,14 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
+    """እባክዎ ወደ my.telegram.org
+ በመሄድ የርስዎን API ID እና API HASH ያግኙ። 
+በመጀመሪያ API DEVELOPMENT TOOL የሚለውን ይንኩ።
+  በመቀጠል የሚጠየቁትን በመሙላት አዲስ Application ይፍጠሩ።
+"""
 )
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(input("በዚህ ቦታ ላይ ከቴሌግራም ያገኙትን APP ID ያስገቡ: "))
+API_HASH = input("በዚህ ቦታ ላይ ከቴሌግራም ያገኙትን API HASH ያስገቡ: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
